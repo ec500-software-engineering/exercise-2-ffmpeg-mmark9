377da7be0dcf0f91f25131bf98f7bba74b1529ef
Miguel Mark <mmark9@bu.edu>
2019-02-25 22:48:27 -0500

    Add non-interactive option as CLI arg
    To facilitate more automated stress testing, having the user provide
    a encoding jobs via file seems more logical than through a REPL
    environment.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 import time
 import queue
 import random
+import argparse
 import multiprocessing
 from utils import encoder
 from threading import Thread
@@ -285,7 +287,7 @@ def run_backend(input_queue, output_queue, num_workers):
         thread_list[i].join()
 
 
-def main():
+def run_interactive():
     a_queue = multiprocessing.Queue()
     b_queue = multiprocessing.Queue()
     backend_proc = Process(
@@ -295,7 +297,81 @@ def main():
     backend_proc.start()
     repl_routine(a_queue, b_queue)
     backend_proc.join()
+    return 0
+
+
+def run_batch_job(job_file):
+    if not os.path.exists(job_file):
+        print(
+            'Failed to open {}; no such file found!'.format(
+                job_file)
+        )
+        return -1
+    a_queue = multiprocessing.Queue()
+    b_queue = multiprocessing.Queue()
+    job_count = 0
+    with open(job_file) as in_fh:
+        print(
+            'Reading jobs from job file {}...'.format(job_file)
+        )
+        for line in in_fh:
+            line_parts = line.rstrip().split(',')
+            p = Packet(PacketType.ENCODE_TASK)
+            p.task = EncodeTask(
+                    line_parts[0],
+                    line_parts[1],
+                    int(line_parts[2]),
+                    None,  # fps is not needed yet...
+                    int(line_parts[3])
+                )
+            b_queue.put(p)
+            job_count += 1
+    print('Read in {} jobs from {}'.format(job_count, job_file))
+    shutdown_packet = Packet(
+        PacketType.COMMAND
+    )
+    shutdown_packet.cmd = Command.SHUTDOWN
+    b_queue.put(
+        shutdown_packet
+    )
+    print('Waiting for {} jobs to finish...'.format(job_count))
+    backend_proc = Process(
+        target=run_backend,
+        args=(b_queue, a_queue, multiprocessing.cpu_count(),)
+    )
+    backend_proc.start()
+    backend_proc.join()
+
+
+def main(cmd_args):
+    if cmd_args['interactive']:
+        return run_interactive()
+    else:
+        return run_batch_job(cmd_args['job_file'])
 
 
 if __name__ == '__main__':
-    main()
+    arg_parser = argparse.ArgumentParser(
+        description=(
+            'EC500 exercise 2 - async file encoding using ffmpeg'
+        )
+    )
+    run_opt_grp = arg_parser.add_mutually_exclusive_group(
+        required=True
+    )
+    run_opt_grp.add_argument(
+        '--interactive',
+        help=(
+            'Run this program interactively. (Uses a REPL environment as the interface)'
+        ),
+        action='store_true'
+    )
+    run_opt_grp.add_argument(
+        '-job_file',
+        help=(
+            'Non interactive mode; provide a list of encoding jobs to process in a CSV file'
+        )
+    )
+    main(
+        vars(arg_parser.parse_args())
+    )
